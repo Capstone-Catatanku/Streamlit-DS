359b9497ec714212b3042be72e524d38c0c768fa
Hafid Dwi Januar <hafiddwi.04@gmail.com>
2026-05-21 14:34:59 +0700

    adding regresion page

--- pages/profil.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def tampilkan_profil():
-    st.title("Ini Halaman Profil")
-    st.write("Halaman Profil ada di sini.")
-
-if __name__ == "__main__":
-    tampilkan_profil()
